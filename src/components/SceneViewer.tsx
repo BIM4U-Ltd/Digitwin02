e0f7480369f360671de62ae16a20b531c55b6f28
sheppied007 <56733233+sheppied007@users.noreply.github.com>
2025-10-17 23:20:55 +0100

    Updated plasmic-init-js and SceneViewer.tsx

---
+++ src/components/SceneViewer.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+interface SceneViewerProps {
+  sceneId?: string;
+}
+
+const SceneViewer: React.FC<SceneViewerProps> = ({ sceneId = 'Home' }) => {
+  const viewerUrl = `https://digitwin01.api.uks.digitaltwins.azure.net/scenesstudio/viewer?sceneId=${sceneId}`;
+
+  return (
+    <iframe
+      src={viewerUrl}
+      width="100%"
+      height="800px"
+      frameBorder="0"
+      allowFullScreen
+      title="Azure 3D Scene"
+    />
+  );
+};
+
+export default SceneViewer;
